46f6a3b49654151c48d680210e8a0f2b42797c10
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2021-10-08 03:02:14 +0200

    klaar voor publicatie

--- functions.php
+++ functions.php
@@ -2,28 +2,30 @@
 
 global $wp_query;
 
-define('KIND_DIR',get_stylesheet_directory());
-define('KIND_URI',get_stylesheet_directory_uri());
+define('KIND_DIR', get_stylesheet_directory());
+define('KIND_URI', get_stylesheet_directory_uri());
 
 //deze klassen extenden die van de parent theme. Moet dus later geladen worden. functions.php wordt geladen in 'setup theme' dus daar wachten we op.
-function kinder_klassen(){
-	include_once KIND_DIR . '/kind-klassen.php';
+function kinder_klassen()
+{
+    include_once KIND_DIR . '/kind-klassen.php';
 }
 
 add_action('after_setup_theme', 'kinder_klassen');
 
+include_once KIND_DIR . '/kind-gereedschap.php';
 include_once KIND_DIR . '/kind-hooks.php';
 include_once KIND_DIR . '/overschrijvingen.php';
 
 
-function agitatie_stijl_en_script() {
-    wp_enqueue_style( 'agitatie-stijl', THEME_URI.'/style.css', array(), null );
-    wp_enqueue_style( 'kind-stijl', get_stylesheet_uri(), array('agitatie-stijl'), null );
-    wp_enqueue_script( 'agitatie-script', JS_URI.'/all.js', array(), null, true );
-    wp_enqueue_script( 'kind-script', KIND_URI.'/js/kind.js', array(), null, true );
+function agitatie_stijl_en_script()
+{
+    wp_enqueue_style('agitatie-stijl', THEME_URI.'/style.css', array(), null);
+    wp_enqueue_style('kind-stijl', get_stylesheet_uri(), array('agitatie-stijl'), null);
+    wp_enqueue_script('kind-script', KIND_URI.'/js/kind.js', array(), null, true);
 }
 
-add_action( 'wp_enqueue_scripts', 'agitatie_stijl_en_script' );
+add_action('wp_enqueue_scripts', 'agitatie_stijl_en_script');
 
 $kind_config = array(
     // 'support'                      => array(
@@ -33,21 +35,21 @@ $kind_config = array(
     //        'flex-width'            => true,
     //     ),
     // ),
-    // 'archief'                      => array(
-    //     'faq'                      => array(
-    //         'geen_afb'             => true,
-    //         'geen_datum'           => true,
-    //         'exc_lim'              => 300
-    //     ),
-    //     'post'                     => array(
-    //         'taxonomieen'          => true
-    //     )
-    // ),
+     'archief'                      => array(
+    'story'                      => array(
+        'geen_afb'             => true,
+        'geen_datum'           => true,
+        'exc_lim'              => 300
+    )
+     ),
+    'post'                     => array(
+        'taxonomieen'          => false
+    )
     // 'content_width'                => 760
 
 );
 $kind_menus = array(
-	//'voorpagina'
+    //'voorpagina'
 );
 
 $kind_thumbs = array(
@@ -61,21 +63,23 @@ $kind_thumbs = array(
 
 
 
-function registreer_posttypes() {
+function registreer_posttypes()
+{
 
     // ter voorbeeld
 
-    // $faq = new Posttype_voorb('faq', 'faqs');
-    // $faq->pas_args_aan(array(
-    //     'menu_icon'           => 'dashicons-editor-quote',
-    // ));
-    // $faq->registreer();
+    $story = new Posttype_voorb('story', 'stories');
+    $story->pas_args_aan(array(
+        'menu_icon'           => 'dashicons-editor-quote',
+        'description'         => "<div class='bericht-tekst'>
+            <p>These are the stories of some of our riders.</p>
+        </div>"
+    ));
+    $story->registreer();
 
     // $download = new Posttype_voorb('download', 'downloads');
     // $download->pas_args_aan(array(
     //     'menu_icon'             => 'dashicons-download'
     // ));
     // $download->registreer();
-
 }
-
--- kind-hooks.php
+++ kind-hooks.php
@@ -1,3 +1,89 @@
 <?php
 
+function ag_vp_print_nieuws_hook()
+{
+    $rider_stories = new WP_Query(array(
+        'post_type' => 'story',
+        'posts_per_page' => 6
+    ));
+    
+    $vp_posts = new WP_Query(array(
+        'posts_per_page' => 6
+    ));
 
+    if (count($rider_stories->posts) > 0) :
+        echo "<section class='vp-nieuws verpakking verpakking-klein'>
+		<h2>Rider stories</h2><div class='art-lijst'>";
+        
+    foreach ($rider_stories->posts as $story) :
+        if (!isset($a)) {
+            $a = new Ag_article_c(array(
+                'class' => 'in-lijst',
+                'htype' => 3,
+                'geen_datum'=> true,
+                'geen_afb' => false
+            ), $story);
+        } else {
+            $a->art = $story;
+        }
+
+    $a->gecontroleerd = false;
+    $a->print();
+
+    endforeach;
+
+    echo "</div>"; //art lijst
+    archive_footer_link('story', 'More stories');
+    echo "</section>";
+        
+    endif; //if stories
+
+
+    echo "<section class='vp-nieuws verpakking verpakking-klein'>
+	<h2>News</h2>";
+
+    if (count($vp_posts->posts)) :
+    echo "<div class='art-lijst'>";
+
+    foreach ($vp_posts->posts as $vp_post) :
+
+        if (!isset($a)) {
+            $a = new Ag_article_c(array(
+                'class' 		=> 'in-lijst',
+                'htype'			=> 3,
+                'geen_tekst'	=> true,
+                'geen_afb'		=> false
+            ), $vp_post);
+        } else {
+            $a->art = $vp_post;
+        }
+
+    $a->gecontroleerd = false;
+    $a->print();
+
+    endforeach;
+    echo "</div>"; //art lijst
+    archive_footer_link('post', 'More news');
+    echo "</section>";
+
+    endif;
+}
+
+function ag_kop_links()
+{
+    echo "<div class='stek-kop-links'><a href='".get_home_url()."' class='radical-riders-title '>Radical Riders</a></div><!--koplinks-->";
+}
+function ag_logo_in_footer_hook()
+{
+    echo "<a href='".get_home_url()."' class='radical-riders-title radical-riders-title--footer'>Radical Riders</a>";
+}
+
+function ag_no_date_stories_single(){
+
+    if (!array_key_exists('story', $_GET)) return;
+
+    remove_action('ag_pagina_voor_tekst', 'ag_print_datum_ctrl');
+    
+}
+
+add_action('after_setup_theme', 'ag_no_date_stories_single');
