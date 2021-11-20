0e7ecb7281d0565113eb65b7296c6f0138777c5a
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2021-11-20 03:34:30 +0100

    21maart init

--- overschrijvingen.php
+++ overschrijvingen.php
@@ -1,1 +1,15 @@
 <?php
+
+function ag_kop_links()
+{
+  $blogtitel = get_bloginfo();
+  $blog_url = site_url();
+  echo "<a href='$blog_url' class='heading-logo'>$blogtitel</a>";
+}
+
+function ag_logo_in_footer_hook()
+{
+  $blogtitel = get_bloginfo();
+  $blog_url = site_url();
+  echo "<a href='$blog_url' class='heading-logo heading-logo--footer'>$blogtitel</a>";
+}
