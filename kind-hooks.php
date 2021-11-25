90054ce46bf980bffb75cf5148b2744efdd6c708
Sjerp van Wouden <dev@sjerpbouwtsites.nl>
2021-11-26 00:36:43 +0100

    gitignore

--- kind-hooks.php
+++ kind-hooks.php
@@ -1,12 +1,14 @@
 <?php
 
+use agitatie\taal as taal;
+
 function ag_vp_print_nieuws_hook()
 {
     $rider_stories = new WP_Query(array(
         'post_type' => 'story',
         'posts_per_page' => 6
     ));
-    
+
     $vp_posts = new WP_Query(array(
         'posts_per_page' => 6
     ));
@@ -14,28 +16,28 @@ function ag_vp_print_nieuws_hook()
     if (count($rider_stories->posts) > 0) :
         echo "<section class='vp-nieuws verpakking verpakking-klein'>
 		<h2>Rider stories</h2><div class='art-lijst'>";
-        
-    foreach ($rider_stories->posts as $story) :
-        if (!isset($a)) {
-            $a = new Ag_article_c(array(
-                'class' => 'in-lijst',
-                'htype' => 3,
-                'geen_datum'=> true,
-                'geen_afb' => false
-            ), $story);
-        } else {
-            $a->art = $story;
-        }
 
-    $a->gecontroleerd = false;
-    $a->print();
+        foreach ($rider_stories->posts as $story) :
+            if (!isset($a)) {
+                $a = new Ag_article_c(array(
+                    'class' => 'in-lijst',
+                    'htype' => 3,
+                    'geen_datum' => true,
+                    'geen_afb' => false
+                ), $story);
+            } else {
+                $a->art = $story;
+            }
 
-    endforeach;
+            $a->gecontroleerd = false;
+            $a->print();
 
-    echo "</div>"; //art lijst
-    archive_footer_link('story', 'More stories');
-    echo "</section>";
-        
+        endforeach;
+
+        echo "</div>"; //art lijst
+        archive_footer_link('story', 'More stories');
+        echo "</section>";
+
     endif; //if stories
 
 
@@ -43,47 +45,47 @@ function ag_vp_print_nieuws_hook()
 	<h2>News</h2>";
 
     if (count($vp_posts->posts)) :
-    echo "<div class='art-lijst'>";
+        echo "<div class='art-lijst'>";
 
-    foreach ($vp_posts->posts as $vp_post) :
+        foreach ($vp_posts->posts as $vp_post) :
 
-        if (!isset($a)) {
-            $a = new Ag_article_c(array(
-                'class' 		=> 'in-lijst',
-                'htype'			=> 3,
-                'geen_tekst'	=> true,
-                'geen_afb'		=> false
-            ), $vp_post);
-        } else {
-            $a->art = $vp_post;
-        }
+            if (!isset($a)) {
+                $a = new Ag_article_c(array(
+                    'class'         => 'in-lijst',
+                    'htype'            => 3,
+                    'geen_tekst'    => true,
+                    'geen_afb'        => false
+                ), $vp_post);
+            } else {
+                $a->art = $vp_post;
+            }
 
-    $a->gecontroleerd = false;
-    $a->print();
+            $a->gecontroleerd = false;
+            $a->print();
 
-    endforeach;
-    echo "</div>"; //art lijst
-    archive_footer_link('post', 'More news');
-    echo "</section>";
+        endforeach;
+        echo "</div>"; //art lijst
+        archive_footer_link('post', 'More news');
+        echo "</section>";
 
     endif;
 }
 
 function ag_kop_links()
 {
-    echo "<div class='stek-kop-links'><a href='".get_home_url()."' class='radical-riders-title '>Radical Riders</a></div><!--koplinks-->";
+    echo "<div class='stek-kop-links'><a href='" . taal\home_url() . "' class='radical-riders-title '>Radical Riders</a></div><!--koplinks-->";
 }
 function ag_logo_in_footer_hook()
 {
-    echo "<a href='".get_home_url()."' class='radical-riders-title radical-riders-title--footer'>Radical Riders</a>";
+    echo "<a href='" . taal\home_url() . "' class='radical-riders-title radical-riders-title--footer'>Radical Riders</a>";
 }
 
-function ag_no_date_stories_single(){
+function ag_no_date_stories_single()
+{
 
     if (!array_key_exists('story', $_GET)) return;
 
     remove_action('ag_pagina_voor_tekst', 'ag_print_datum_ctrl');
-    
 }
 
 add_action('after_setup_theme', 'ag_no_date_stories_single');
