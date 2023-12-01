974ae3ba5d3b45483c3fde8dc47540ca84854d86
Sjerp van Wouden <ik@sjerpvanwouden.nl>
2023-12-01 09:51:03 +0100

    dev

--- kind-hooks.php
+++ kind-hooks.php
@@ -49,10 +49,12 @@ if (!function_exists('ag_vp_print_nieuws_hook')) : function ag_vp_print_nieuws_h
 
             $text = get_the_excerpt();
             $matches= preg_match(
-                "/^.{0,250}\s/gm",
-                $text
+                "/^.{0,250}\s/",
+                $text,
+                null,
+                'gm'
             );
-            if (count($matches)) {
+            if (isArray($matches) && count($matches)) {
                 $text = $matches[0];
             }
             return array(
