962951a5c417d57221acb198300ae1e9b5c1f659
Sjerp van Wouden <ik@sjerpvanwouden.nl>
2023-12-01 09:58:21 +0100

    dev

--- kind-hooks.php
+++ kind-hooks.php
@@ -48,7 +48,7 @@ if (!function_exists('ag_vp_print_nieuws_hook')) : function ag_vp_print_nieuws_h
             $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
 
             $text = get_the_excerpt();
-            $text = substr($text, 0, strrpos($text, ' '));
+            $text = substr(substr($text, 120), 0, strrpos($text, ' '));
             // if (is_array($matches)) {
             //     $text = $matches[0];
             // } else {
