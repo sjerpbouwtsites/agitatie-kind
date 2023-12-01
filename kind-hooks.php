ff698e8f895b8f119b0079b514d9e84604e568c6
Sjerp van Wouden <ik@sjerpvanwouden.nl>
2023-12-01 03:04:17 +0100

    dev

--- kind-hooks.php
+++ kind-hooks.php
@@ -27,6 +27,76 @@ if (!function_exists('ag_archief_content_hook')) : function ag_archief_content_h
 }
 endif;
 
+if (!function_exists('ag_vp_print_nieuws_hook')) : function ag_vp_print_nieuws_hook()
+{
+    $vp_posts = new WP_Query(array(
+        'posts_per_page' => 5
+    ));
+
+    if (count($vp_posts->posts)) :
+
+        $footerknop = new Ag_knop(array(
+            'link' 		=> get_post_type_archive_link('post'),
+            'tekst' 	=> ucfirst(\agitatie\taal\streng('alle')) . ' ' . \agitatie\taal\streng('posts'),
+            'class'		=> 'in-wit'
+        ));
+
+        echo "<section class='vp-nieuws verpakking'>
+				<h2>" . ucfirst(\agitatie\taal\streng('nieuws')) . "</h2>
+					<div class='art-lijst'>";
+
+        foreach ($vp_posts->posts as $vp_post) :
+            if (!isset($a)) {
+                $a = new Ag_article_c(array(
+                    'class' 		=> 'in-lijst',
+                    'htype'			=> 3,
+                    'geen_afb'		=> false
+                ), $vp_post);
+            } else {
+                $a->art = $vp_post;
+            }
+            $a->gecontroleerd = false;
+
+            $a->print();
+        endforeach;
+
+    echo "</div>"; //art lijst
+    echo "<footer>";
+    $footerknop->print();
+    echo "</footer>";
+
+    echo "</section>";
+
+    endif;
+}
+endif;
+
+add_action('voorpagina_na_tekst_action', 'ag_vp_print_nieuws_hook', 20);
+
+function oyvey_vp_agenda()
+{
+    //$afm = ag_agenda_filter_ctrl();
+
+    $agenda = new Ag_agenda(array(
+        'aantal' => 10,
+        'omgeving' => 'pagina'
+    ));
+
+    if (count($agenda->agendastukken) > 0) :
+
+        echo "<section class='verpakking verpakking-klein marginveld'>";
+        echo "<div class='agenda'>
+			<h2>Agenda</h2>";
+
+        $agenda->print();
+        echo "</div>";
+        echo "</section>";
+
+    endif; // als agendastukken
+}
+
+add_action('voorpagina_na_tekst_action', 'oyvey_vp_agenda', 25);
+
 // function vervang_singular_na_artikel(){
 // 	remove_action('ag_singular_na_artikel', 'ag_singular_taxonomieen', 20);
 // 	add_action('ag_singular_na_artikel', 'ag_vp_print_nieuws_hook' );
