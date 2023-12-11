0fb07c1f06708d9e411102929dd12e79e1484b54
Sjerp van Wouden <ik@sjerpvanwouden.nl>
2023-12-11 02:52:02 +0100

    dev

--- resources/build/js/kind-bundel.js
+++ resources/build/js/kind-bundel.js
@@ -15,47 +15,5 @@ function copyCloseMenuForAccessibility(){
 
 window.addEventListener('load', copyCloseMenuForAccessibility);
 
-function setHexagonsOnHero(){
-
-if (!document.body.classList.contains('home')) return
-
-const hexagonsOuter = document.createElement('div');
-hexagonsOuter.className = 'hexagons-outer';
-let singleHTML = '';
-const bodyWidth = document.body.offsetWidth;
-let hexagonCount = bodyWidth > 1500 ? 36 : bodyWidth > 1200 ? 21 : bodyWidth > 768 ? 16 : 10;
-
-for (let i = 0; i < hexagonCount; i+=1){
-singleHTML +=`<div id='hexagons-single-${i+1}' class="hexagons-single">
-<div  class='hexagons-inner'></div>
-</div>`;
-}
-hexagonsOuter.innerHTML = `<div class="hexagons-container">${singleHTML}</div>`;
-  
-  document.querySelector('.uitgelichte-afbeelding-buiten.hero').appendChild(hexagonsOuter);
-
-  setTimeout(()=>{
-    addClassesToHexagon(hexagonCount);
-  }, 100);
-
-}
-
-function addClassesToHexagon(hexagonCount){
-    const thisEl = document.getElementById(`hexagons-single-${hexagonCount}`);
-    
-        thisEl.classList.add('visible');
-    
-    setTimeout(()=>{
-        thisEl.classList.add('rotated');
-    }, 250);    
-
-    if (hexagonCount > 1){
-        const newcount = hexagonCount -1;
-        setTimeout(()=>{
-            addClassesToHexagon(newcount);
-        }, 8);
-    }
-
-}
-
-window.addEventListener('load', setHexagonsOnHero);
+console.log('sexHexagonsOnHero uitgecommentarieerd index.js agitatie-kind uncompiled js');
+//window.addEventListener('load', setHexagonsOnHero)
