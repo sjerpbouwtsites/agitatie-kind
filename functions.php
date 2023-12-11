d910c1f1ff8acdba9adb440049c42aa6fdc4113f
Sjerp van Wouden <ik@sjerpvanwouden.nl>
2023-12-11 13:52:21 +0100

    dev

--- functions.php
+++ functions.php
@@ -405,3 +405,21 @@ function create_hexagon_grid($hexagon_list)
 
     echo "</section>";
 }
+
+function enable_svg_upload($upload_mimes)
+{
+    $upload_mimes['svg'] = 'image/svg+xml';
+
+
+
+    $upload_mimes['svgz'] = 'image/svg+xml';
+
+
+
+    return $upload_mimes;
+}
+
+
+
+
+add_filter('upload_mimes', 'enable_svg_upload', 10, 1);
