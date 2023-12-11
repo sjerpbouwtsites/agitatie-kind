97ca2f5ecabd606c10407fe126dc91ff9783ff42
Sjerp van Wouden <ik@sjerpvanwouden.nl>
2023-12-11 10:23:19 +0100

    dev

--- resources/build/js/kind-bundel.js
+++ resources/build/js/kind-bundel.js
@@ -57,16 +57,16 @@ x8  9   10  x11
 `.match(/(x\d+)/g).map(a=>Number(a.replace('x','')));
 
 const hideIndexAboveWidth900 = `
-1   2   3    
-    4   5      
-6   7   8    
-    9   10   
-11  12  13   
-    14  15   
-16  17   18   
-    19   20   
-21  22   23  
-24  25   x26 
+x1  x2  x3  4  
+    x5  6   7   
+x8  9   10  x11  
+    12  x13 14  
+15  x16 x17 x18  
+    19  20  x21 
+22  23  x24 25  
+    26  x27 28  
+29  30  x31 32 
+33  x34 x35 x36
 `.match(/(x\d+)/g).map(a=>Number(a.replace('x','')));
 
 
