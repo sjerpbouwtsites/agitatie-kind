4edb4445ad1391cf0bc5360292bdf978ad674f96
Sjerp van Wouden <ik@sjerpvanwouden.nl>
2023-12-18 20:32:23 +0100

    dev

--- functions.php
+++ functions.php
@@ -131,13 +131,13 @@ function create_oyvey_team()
     $team = new Posttype_voorb('team', 'teams');
 
     $team->pas_args_aan(array(
-        'publicly_queryable' => false,
+        'publicly_queryable' => true,
         'show_in_nav_menus'	=> true,
         'show_in_menu'		=> true,
         'add_to_menu'		=> true,
         'public'			=> true,
         'has_archive' 		=> true,
-        'exclude_from_search' => true,
+        // 'exclude_from_search' => true,
         'supports' =>
             array(
                 'title',
