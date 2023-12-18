100426119e8df497f13fac0cd661c95f2c840c50
Sjerp van Wouden <ik@sjerpvanwouden.nl>
2023-12-18 19:00:36 +0100

    dev

--- page-with-cards.php
+++ page-with-cards.php
@@ -52,7 +52,12 @@ while (have_posts()) : the_post();
                 $a = new Ag_article_c(array(
                     'class' 		=> 'in-lijst',
                     'htype'			=> 3,
+                    'geen_afb'      => true,
+                    'geen_datum'    => true,
                     'is_categorie'	=> true,
+                    'geen_meer_tekst'=> true,
+                    'geen_tekst'=> true,
+                    'korte_titel'=> true
                 ), $soort);
                 $a->print();
                 echo "</div>";
