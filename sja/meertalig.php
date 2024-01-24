1d3b7ec3f69b3057d12ef69138f6895d1cb80863
Sjerp van Wouden <ik@sjerpvanwouden.nl>
2024-01-24 06:51:52 +0100

    a

--- sja/meertalig.php
+++ sja/meertalig.php
@@ -7,5 +7,14 @@ if (function_exists('pll_the_languages')) :
     pll_the_languages(array('show_flags' => 0, 'show_names' => 1, 'display_names_as'=>'slug','dropdown'=> 1));
     echo "</ul>";
     echo "</div>";
-
+    echo "<script>
+    document.querySelectorAll('#lang_choice_1 option').forEach(option => {
+      if (option.textContent.includes('nl')) {
+        option.setAttribute('aria-label', 'Nederlands');
+      }
+      if (option.textContent.includes('en')) {
+        option.setAttribute('aria-label', 'English');
+      }      
+    });
+  </script>";
 endif; // pll_the_languages exists
