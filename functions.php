bb5652bdc957c8b14f05892324d70b019b81fdd2
Sjerp van Wouden <ik@sjerpvanwouden.nl>
2024-01-24 06:09:12 +0100

    a

--- functions.php
+++ functions.php
@@ -595,7 +595,8 @@ function smartwp_remove_wp_block_library_css()
 {
     wp_dequeue_style('wp-block-library');
     wp_dequeue_style('wp-block-library-theme');
-    wp_dequeue_style('mediaelementplayer-legacy');
+    wp_deregister_script('wp-mediaelement');
+    wp_deregister_style('wp-mediaelement');
     wp_dequeue_style('classic-themes');
     wp_dequeue_style('wc-blocks-style');
 }
