f95136f6070c2d3a19849193a83959ca2736e326
Sjerp van Wouden <ik@sjerpvanwouden.nl>
2024-01-24 06:07:11 +0100

    a

--- functions.php
+++ functions.php
@@ -590,3 +590,11 @@ function jeherve_remove_all_jp_css()
     echo "<script>console.log(".json_encode($wp_styles).");</script>";
 }
 add_action('wp_print_styles', 'jeherve_remove_all_jp_css', 99);
+
+function smartwp_remove_wp_block_library_css()
+{
+    wp_dequeue_style('wp-block-library');
+    wp_dequeue_style('wp-block-library-theme');
+    wp_dequeue_style('wc-blocks-style'); // Remove WooCommerce block CSS
+}
+add_action('wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100);
