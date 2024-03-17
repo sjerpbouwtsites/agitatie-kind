86cf944700290701f47e4853641e4c003313a238
Sjerp van Wouden <ik@sjerpvanwouden.nl>
2024-03-17 03:38:24 +0100

    locatie opties

--- kind-agenda-hooks.php
+++ kind-agenda-hooks.php
@@ -25,19 +25,38 @@ if (!function_exists('ag_agenda_plek_adres')) :
     {
         global $post;
 
+        $locatie_optie = get_field('locatie_optie', $post->ID);
+
+        if ($locatie_optie === 'online') {
+            echo "<address class='agenda-address'>";
+            echo "<h3 class='agenda-address__titel tekst-zijkleur serif-letter'>Online event</h3>";
+            echo "<span class='agenda-address__regel'".\agitatie\taal\streng('Locatie wordt gemaild')."</span>";
+            echo "</address>";
+            return;
+        }
+
         $plekken = wp_get_post_terms($post->ID, 'locatie');
         if (count($plekken) < 1) {
             return;
         }
 
         $p = $plekken[0];
+        $stad = get_field('stad', 'locatie_' . $p->term_id);
+
+        if ($locatie_optie === 'prive') {
+            echo "<address class='agenda-address'>";
+            echo "<h3 class='agenda-address__titel tekst-zijkleur serif-letter'>".\agitatie\taal\streng('Privé event')." in  $stad</h3>";
+            echo "<span class='agenda-address__regel'>".\agitatie\taal\streng('Locatie wordt gemaild')."</span>";
+            echo "</address>";
+            return;
+        }
+
         $straat = get_field('straat', 'locatie_' . $p->term_id);
         $huisnummer = get_field('huisnummer', 'locatie_' . $p->term_id);
-        $stad = get_field('stad', 'locatie_' . $p->term_id);
         $tijd = get_field('datum');
 
         echo "<address class='agenda-address'>";
-        echo "<h3 class='agenda-address__titel tekst-zijkleur serif-letter'>$p->name $stad</h3>";
+        echo "<h3 class='agenda-address__titel tekst-zijkleur serif-letter'>$p->name in $stad</h3>";
         echo "<span class='agenda-address__regel'>$straat $huisnummer</span>";
         echo "<span class='agenda-address__regel'>$tijd</span>";
         echo "</address>";
